73bd9fe04f4a7b94f98cd1bc0a8e555ce2204e10
dineshbalani <32188916+dineshbalani@users.noreply.github.com>
2017-10-21 14:42:32 -0400

    Handled timers, hashing, multiple clients

--- dinesh file/parseOperation.py
+++ dinesh file/parseOperation.py
@@ -1,22 +1,38 @@
 data = dict()
 
 def addToDictionary(key,val):
-    data[key]=val
-    return 'OK'
-def getFrom(key):
+    try:
+        print(key)
+        data[key]=val
+        return 'OK'
+    except:
+        return 'Fail'
+
+def getFrom(key,val):
     try:
         return data[key]
     except:
         return ''
 
 def appendDictionary(key,val):
-    data[key] = val
-    return 'OK'
+    try:
+        valueExsist = data[key]
+        data[key] = val
+        return 'OK'
+    except:
+        return 'fail'
 
 def sliceDictionary(key,val):
-    value = data[key]
-    data[key] = value[val]
-    return 'OK'
+    try:
+        print('key',key)
+        print(data)
+        value = data[key]
+        indices = val.split(':')
+        print(value)
+        data[key] = value[int(indices[0]):int(indices[1])]
+        return 'OK'
+    except:
+        return 'fail'
 
 def executeOperation(operation,key,val):
     function_launch = {
@@ -32,6 +48,7 @@ def parseString(inp2):
     inp = str(inp2,'utf-8')[:-1]
     opKeyVal = inp.split('(')
     operation = opKeyVal[0]
+    print('operation')
     keyVal = opKeyVal[1].split(',')
     key = keyVal[0][1:-1]
     value = keyVal[1][1:-1]
@@ -48,14 +65,17 @@ def doOperation(inp):
     print(ret)
 
 
+#put('jedi,'luke skywalker); slice('jedi','0:4'); get('jedi')
+inp = b"put('jedi','luke skywalker')"
+doOperation(inp)
+print(data)
+inp = b"slice('jedi','3:10')"
+doOperation(inp)
+print(data)
+inp = b"get('jedi')"
+doOperation(inp)
 
-inp = b"put('movie','star')"
-doOperation(inp)
-print(data)
-inp = b"append('movie',' wars')"
-doOperation(inp)
-print(data)
-inp = b"get('movie')"
+
 print(data)
 
 
